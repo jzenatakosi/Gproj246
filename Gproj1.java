33caba3373ac17db9e10323b693c044b5efb094f
jzenatakosi <ngambwa03@gmail.com>
2022-09-21 15:35:26 +0300

    ane more changes

--- Gproj1.java
+++ Gproj1.java
@@ -3,6 +3,7 @@ package Jv1;
 public class Gproj1 {
     public static void main(String[] args) {
         System.out.printf("Hello my dear people");
+        System.out.printf("the commit is active");
 
 
     }
