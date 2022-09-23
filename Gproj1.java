01c4d97114f99075248bab3db686940fc1078335
jzenatakosi <ngambwa03@gmail.com>
2022-09-23 14:20:19 +0300

    added some arithmetics

--- Gproj1.java
+++ Gproj1.java
@@ -5,7 +5,11 @@ public class Gproj1 {
         System.out.printf("Hello my dear people");
         System.out.printf("the commit is active");
         System.out.printf("the commit3");
-        System.out.printf("the commit4");
+        System.out.println("the commit4");
+        int a=3;
+        int b=5;
+        int c=(a+b)/2;
+        System.out.println(c);
 
 
     }
